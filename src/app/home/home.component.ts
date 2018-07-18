c87612c8decfe569470b14a7daa13222aa391feb
eleganceinfo <atulmali001@gmail.com>
2018-07-18 18:11:34 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { analyzeAndValidateNgModules } from '../../../node_modules/@angular/compiler';
+import { NgModel } from '../../../node_modules/@angular/forms';
+import {trigger,style,transition,animate,keyframes,query,stagger, animation} from '@angular/animations';
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations:[
+
+      trigger('goals',[
+
+        transition('*=>*',[
+          query(':enter',style({opacity:0}),{optional:true}),
+          query(':enter',stagger('300ms',[
+            animate('.6s ease-in',keyframes([
+                style({opacity:0,transform:'translateY(-75%)',offset:0}),
+                style({opacity:.5,transform:'translateY(35px)',offset:.3}),
+                style({opacity:0,transform:'translateY(0)',offset:1}),
+            ]))]),{optional:true}),
+            
+            query(':leave',stagger('300ms',[
+              animate('.6s ease-in',keyframes([
+                  style({opacity:1,transform:'translateY(0)',offset:0}),
+                  style({opacity:.5,transform:'translateY(35px)',offset:.3}),
+                  style({opacity:0,transform:'translateY(-75%)',offset:1}),
+              ]))]),{optional:true})
+        ])
+
+      ])
+
+  ]
+    
+})
+
+export class HomeComponent implements OnInit {
+
+  itemCount : number;
+  btnText:string="Add an item";
+  goalText:string="My First Goal.";
+  goals=[];
+  constructor() { }
+
+  ngOnInit() {
+
+    this.itemCount=this.goals.length;
+    
+  }
+
+  addItem(){
+this.goals.push(this.goalText);
+this.goalText='';
+this.itemCount=this.goals.length;
+
+
+  }
+
+  removeItem(i){
+
+    this.goals.splice(i,1);
+   
+
+  }
+
+  
+
+}
